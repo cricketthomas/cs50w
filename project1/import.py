ef2a94d85328e2fe8f2bd9304364818bc2109980
Arthur Thomas <acthomasiv@gmail.com>
2019-02-21 19:05:26 -0500

    finished importing books csv file

--- project1/import.py
+++ project1/import.py
@@ -1,6 +1,5 @@
 import csv
 import os
-import pandas as pd
 from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -20,21 +19,16 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-#open books.csv
-books = csv.reader(file("books.csv"))
-
-
-
 
 try:
     db.execute("""
         CREATE TABLE books (
-        isbn INTEGER PRIMARY KEY,
+        isbn VARCHAR PRIMARY KEY,
         title VARCHAR NOT NULL,
-        author VARCHAR NOT NULL, 
-        year INTEGER );
+        author VARCHAR NOT NULL,
+        year VARCHAR NOT NULL);
         """
-        )
+               )
     db.commit()
     print("table created.")
 except:
@@ -42,4 +36,28 @@ except:
 
 
 
+
+# open books.csv
+
+f = open("books.csv")
+reader = csv.reader(f)
+for isbn, title, author, year in reader: # loop gives each column a name
+    db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+              {"isbn": isbn, "title": title, "author": author,"year": year}) # substitute values from CSV line into SQL command, as per this dict
+    print(f"Added book {title} by {author} with ISBN of {isbn} from {year}.")
+    db.commit() # transactions are assumed, so close the transaction finished
+
+
+
+
+
+
 # export DATABASE_URL="postgres://zwtbynvdmuhqzj:8ef8a4d9bc771976d0ee3ed1b09fae82072726642d8ba4593d393baac502803a@ec2-54-163-246-159.compute-1.amazonaws.com:5432/d14i40bolicsn5"
+"""
+sql = "INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+{"isbn": books['isbn'], "title": books['title'], "author": books['author'], "year": books['year'] }
+
+
+("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+               {"isbn": books['isbn'], "title": books['title'], "author": books['author'], "year": books['year']})
+"""
