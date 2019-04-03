5aca03d8018957238a3bce056cf205f68edf6268
Arthur Thomas IV <acthomasiv@gmail.com>
2019-04-03 15:55:09 -0400

    Update application.py

--- project1/application.py
+++ project1/application.py
@@ -102,8 +102,9 @@ def loggedin():
 @app.route('/logout')
 def logout():
     # remove the username from the session if it is there
-    session.pop('username', None)
+    #session.pop('username', None)
     session.clear()
+    session['logged_in'] = False
     return render_template('login.html', message='bye')
 
 
