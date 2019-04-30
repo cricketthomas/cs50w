ebea52e8c590bb3335a93ec8240a68508789fd60
Arthur Thomas IV <acthomasiv@gmail.com>
2019-04-30 15:54:51 -0400

    add size option model

--- project3/orders/admin.py
+++ project3/orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Pizza, Topping_option, Size_option
 # Register your models here.
+admin.site.register(Topping_option)
+admin.site.register(Size_option)
+admin.site.register(Pizza)
