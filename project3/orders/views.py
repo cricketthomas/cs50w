9baa23961f6cbac347c954b7ebd1fd9e98ade67f
Arthur Thomas IV <acthomasiv@gmail.com>
2019-05-03 16:21:29 -0400

    add firstname lastname requirement

--- project3/orders/views.py
+++ project3/orders/views.py
@@ -35,15 +35,22 @@ def register_view(request):
     username = None
     email = None
     password = None
+    first_name = None
+    last_name = None
     if request.POST:
         username = request.POST.get('username')
         password = request.POST.get('password')
         email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
         if User.objects.filter(username=username).exists():
             return render(request, "orders/register.html", {"message": "Username exists."})
         else:
             user = User.objects.create_user(
                 username=username, password=password, email=email)
+            user.first_name = first_name
+            user.last_name = last_name
+
             user.save()
             user = authenticate(request, username=username, password=password)
         if user is not None:
