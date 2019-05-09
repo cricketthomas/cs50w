ebf4ab38423541935aaa7041babdb791a5192fdf
Arthur Thomas IV <acthomasiv@gmail.com>
2019-05-09 16:51:53 -0400

    made sure form was able to submit, lots of work, to be done

--- project3/orders/forms.py
+++ project3/orders/forms.py
@@ -35,4 +35,4 @@ class PlatterForm(forms.ModelForm):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ["pizza", "sub", "platter", "salad", "pasta"]
+        fields = ["user", "pizza", "sub", "platter", "salad", "pasta"]
